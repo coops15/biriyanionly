38877c94e85c559ec8f613297f71af9b732f9028
coops15 <worldhidden0@gmail.com>
2024-06-02 22:09:49 +0530

    update files

--- src/app/menuitems/page.js
+++ src/app/menuitems/page.js
@@ -3,12 +3,15 @@ import Link from "next/link"
 import MenuTap from "../compoents/layout/MenuTap";
 import Icons from "../compoents/layout/Icons";
 import Rightarrow from "../compoents/layout/Rightarrow";
-import { useState } from "react";
+import { act, useState } from "react";
 import mongoose from "mongoose";
 import { Menuitem } from "../../models/Menuitem";
 import Image from "next/image";
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import { storage } from '../../firebaseConfig'
+import Delete from "../compoents/layout/Delete";
+import Edit from "../compoents/layout/Edit";
+import AddItem from "../compoents/layout/AddItem";
 
 export default function MenuItems() {
     const [display, setDisplay] = useState(false)
@@ -18,12 +21,14 @@ export default function MenuItems() {
     const [imageurl, setImageUrl] = useState('')
     const [success, setSuccsess] = useState(false)
     const [saving, setSaving] = useState('')
-const [imagename,setImagename]=useState('avathar')
+    const [imagename, setImagename] = useState('avathar')
+    const [action, setAction] = useState('')
+    const [deletepermission,setDeletePermission]=useState(false)
 
     async function handleFileChange(e) {
         e.preventDefault();
         const file = e.target.files[0]
-setImagename(file.name)
+        setImagename(file.name)
         if (file) {
             try {
 
@@ -41,8 +46,10 @@ setImagename(file.name)
         }
     }
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+    async function handleSubmit(act) {
+        
+        if (act==='ADD') {
+            console.log('correct');
         setSaving('Processing....')
         setSuccsess(true)
         if (imageurl === '') {
@@ -77,13 +84,31 @@ setImagename(file.name)
         } catch (error) {
             console.log(error);
         }
-
-
+        setDisplay(false)
+    }
     }
 
     function handleView(e) {
         e.preventDefault();
         setDisplay(pre => !pre)
+        setAction('ADD')
+        setBiriyaniName('')
+        setDiscription('')
+        setPrice('')
+    }
+
+    function handleEdit(e){
+        e.preventDefault()
+        setAction('EDIT')
+        setBiriyaniName('Test')
+        setDiscription('Test')
+        setPrice('Test')
+        setDisplay(true)
+       
+    }
+
+    function handleDelete(){
+
     }
     return (
         <>
@@ -106,45 +131,108 @@ setImagename(file.name)
                             back to menu list items
                         </div>
                     }</div>
-                {display && <form className="relative max-w-md mx-auto border" onSubmit={handleSubmit}>
+                {display && <AddItem
+                    success={success}
+                    setBiriyaniName={setBiriyaniName}
+                    setDiscription={setDiscription}
+                    setPrice={setPrice}
+                    imageurl={imageurl}
+                    imagename={imagename}
+                    handleFileChange={handleFileChange}
+                    handleSubmit={handleSubmit}
+                    action={action}
+                    saving={saving}
+                    biriyaniname={biriyaniname}
+            discription={discription}
+            price={price}
+                />
+                    // <form className="relative max-w-md mx-auto border" onSubmit={handleSubmit}>
 
-                    {success && (
-                        <div className="flex flex-col items-center justify-center w-100">
-                            <div className="mt-4 text-center  bg-lime-400 p-3 rounded-lg text-white font-semibold">
-                                {saving}
+                    //     {success && (
+                    //         <div className="flex flex-col items-center justify-center w-100">
+                    //             <div className="mt-4 text-center  bg-lime-400 p-3 rounded-lg text-white font-semibold">
+                    //                 {saving}
+                    //             </div>
+                    //         </div>)}
+                    //     <div className="flex gap-2 p-4">
+                    //         <div className="items-center flex flex-col mt-4">
+                    //             <div className="bg-gray-100 p-1 justify-center items-center flex rounded-lg">
+                    //                 <Image src={imageurl} width={80} height={80} alt={imagename} className="rounded-lg" />
+                    //             </div>
+                    //             <div className="relative flex flex-col h-7 items-center w-20 mt-3">
+                    //                 <span className="absolute outline-0 p-4 font-semibold text-sm py-1 cursor-pointer rounded-lg border">Upload Image</span>
+                    //                 <input onChange={handleFileChange} className="absolute w-14 opacity-0 cursor-pointer" type="file" accept="image/*" id="imageUpload" />
+                    //             </div>
+                    //         </div>
+                    //         <div className="flex flex-col">
+                    //             <label className="m-2">
+                    //                 <span className="text-sm text-gray-400 ml-1">biriyani name</span>
+                    //                 <input className="m-0" type="text" onChange={(e) => setBiriyaniName(e.target.value)} placeholder="biriyani Name" />
+                    //             </label>
+                    //             <label className="m-2">
+                    //                 <span className="text-sm text-gray-400 ml-1">discription</span>
+                    //                 <input type="text" onChange={(e) => setDiscription(e.target.value)} placeholder="discription" />
+                    //             </label>
+                    //             <label className="m-2">
+                    //                 <span className="text-sm text-gray-400 ml-1">price</span>
+                    //                 <input type="text" onChange={(e) => setPrice(e.target.value)} placeholder="price" />
+                    //             </label>
+                    //             <button type="submit" className="outline-0 bg-primary text-white font-semibold uppercase py-2 px-4 rounded-lg w-80">Add</button>
+                    //         </div>
+                    //     </div>
+
+                    // </form>
+                }
+
+
+                {!display &&
+                    <div className="w-96">
+                        <span className="font-semibold text-sm text-gray-700">menuitems</span>
+                        <ol className="flex flex-col border relative">
+                           {deletepermission && <div className="bg-white  border rounded-lg shadow-md shadow-gray-500 h-24 w-60 absolute left-20 top-1 p-6 flex flex-col gap-2 justify-center">
+                                <div className="text-sm text-gray-900 items-center">
+                                    Are yor sure want to DELETE this item?
+                                </div>
+                                <div className="flex flex-row justify-between">
+                                    <button onClick={()=>setDeletePermission(false)} className="outline-0 text-sm bg-red-500 text-white px-2 py-1 rounded-lg">
+                                        No
+                                    </button>
+                                    <button onClick={handleDelete} className="outline-0 text-sm bg-lime-500 text-white px-2 py-1 rounded-lg">
+                                        Yes
+                                    </button>
+                                </div>
                             </div>
-                        </div>)}
-                    <div className="flex gap-2 p-4">
-                        <div className="items-center flex flex-col mt-4">
-                            <div className="bg-gray-100 p-1 justify-center items-center flex rounded-lg">
-                                <Image src={imageurl} width={80} height={80} alt={imagename} className="rounded-lg" />
-                            </div>
-                            <div className="relative flex flex-col h-7 items-center w-20 mt-3">
-                                <span className="absolute outline-0 p-4 font-semibold text-sm py-1 cursor-pointer rounded-lg border">Upload Image</span>
-                                <input onChange={handleFileChange} className="absolute w-14 opacity-0 cursor-pointer" type="file" accept="image/*" id="imageUpload" />
-                            </div>
-                        </div>
-                        <div className="flex flex-col">
-                            <label className="m-2">
-                                <span className="text-sm text-gray-400 ml-1">biriyani name</span>
-                                <input className="m-0" type="text" onChange={(e) => setBiriyaniName(e.target.value)} placeholder="biriyani Name" />
-                            </label>
-                            <label className="m-2">
-                                <span className="text-sm text-gray-400 ml-1">discription</span>
-                                <input type="text" onChange={(e) => setDiscription(e.target.value)} placeholder="discription" />
-                            </label>
-                            <label className="m-2">
-                                <span className="text-sm text-gray-400 ml-1">price</span>
-                                <input type="text" onChange={(e) => setPrice(e.target.value)} placeholder="price" />
-                            </label>
-                            <button type="submit" className="outline-0 bg-primary text-white font-semibold uppercase py-2 px-4 rounded-lg w-80">Add</button>
-                        </div>
+}
+                            <li className="flex flex-row gap-5 p-1">
+                                <div className="bg-gray-100 p-1 justify-center items-center flex rounded-lg">
+                                    <Image src={''} width={60} height={60} alt={''} className="rounded-lg" />
+                                </div>
+                                <div className="flex gap-8 font-semibold text-sm items-center">
+                                    <span>Item name</span>
+                                    <span>price</span>
+                                    <button className="outline-0" onClick={handleEdit}><Edit/></button>
+                                    <button className="outline-0" onClick={()=>setDeletePermission(true)}><Delete/></button>
+                                </div>
+
+                            </li>
+                            <li className="flex flex-row gap-5 p-1">
+                                <div className="bg-gray-100 p-1 justify-center items-center flex rounded-lg">
+                                    <Image src={''} width={60} height={60} alt={''} className="rounded-lg" />
+                                </div>
+                                <div className="flex gap-8 font-semibold text-sm items-center">
+                                    <span>Item name</span>
+                                    <span>price</span>
+                                    <Edit />
+                                    <Delete />
+                                </div>
+
+                            </li>
+                        </ol>
                     </div>
-
-                </form>
                 }
 
             </div>
+
         </>
     )
 }
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import MenuTap from "../compoents/layout/MenuTap";
+import Image from "next/image";
 
 export default function Users(){
     return(
@@ -9,6 +10,34 @@ export default function Users(){
                 <Link href='/menuitems'><MenuTap tittle={'Menu Items'} active={'false'}/></Link>
                 <Link href='/users'><MenuTap tittle={'Users'} active={'true'}/></Link>
             </div>
+            <div className="w-full flex items-center justify-center">
+            <div className="w-96">
+                        <span className="font-semibold text-sm text-gray-700">users list</span>
+                        <ol className="flex flex-col border relative">
+                           <li className="flex flex-row gap-5 p-1">
+                                <div className="bg-gray-100 p-1 justify-center items-center flex rounded-lg">
+                                    <Image src={''} width={60} height={60} alt={''} className="rounded-lg" />
+                                </div>
+                                <div className="flex gap-8 text-sm items-center">
+                                    <span className="font-semibold">User name</span>
+                                    <span><i>Email address</i></span>
+                                </div>
+
+                            </li>
+                            <li className="flex flex-row gap-5 p-1">
+                                <div className="bg-gray-100 p-1 justify-center items-center flex rounded-lg">
+                                    <Image src={''} width={60} height={60} alt={''} className="rounded-lg" />
+                                </div>
+                                <div className="flex gap-8 text-sm items-center">
+                                    <span className="font-semibold">User name</span>
+                                    <span><i>Email address</i></span>
+                                </div>
+
+                            </li>
+                        </ol>
+                    </div>
+                    </div>
+        
         </>
     )
 }
